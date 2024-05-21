eba1353543d3021181ea151a2e7b25b46bf89be9
Rameshwar Dudhe <rndudhe1808@gmail.com>
2024-05-21 13:54:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,21 @@
-yum install rpmconf epel-release yum-utils -y
-package-cleanup --orphans
-package-cleanup --leaves
-yum autoremove -y
-yum install dnf -y
-dnf remove yum yum-metadata-parser -y #if condition if yum not there skip actions
+yum -y update && yum -y upgrade 
+yum install epel-release -y
+yum -y install rpmconf  yum-utils
+rpmconf -a
+package-cleanup --leaves; package-cleanup --orphans
+reboot
+yum -y install dnf
+dnf -y remove yum yum-metadata-parser
 rm -Rf /etc/yum
-dnf upgrade -y
-dnf clean all -y
-dnf install -y http://vault.centos.org/centos/8/BaseOS/x86_64/os/Packages/{centos-linux-repos-8-3.el8.noarch.rpm,centos-linux-release-8.5-1.2111.el8.noarch.rpm,centos-gpg-keys-8-3.el8.noarch.rpm}
-dnf -y upgrade -y  https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+dnf -y update
+dnf install -y  http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/{centos-linux-repos-8-3.el8.noarch.rpm,centos-linux-release-8.5-1.2111.el8.noarch.rpm,centos-gpg-keys-8-3.el8.noarch.rpm}
+dnf -y upgrade https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf clean all
+rpm -e `rpm -q kernel`; rpm -e --nodeps sysvinit-tools
+dnf -y --releasever=8 --allowerasing --setopt=deltarpm=false distro-sync
+dnf -y install kernel-core kernel
+dnf install grub2 -y
+dnf update -y
+dnf install centos-release-stream -y
+dnf swap centos-{linux,stream}-repos -y
+reboot
